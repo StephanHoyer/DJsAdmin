caa33af8c2f62779450c5a155bf92b4ac5b046f3
Stephan Hoyer <stephan@noname.(none)>
2009-05-12 00:17:12 +0200

    after running create-application-script

---
+++ application/DJsAdmin/source/class/djsadmin/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("djsadmin.theme.Theme",
+{
+  meta :
+  {
+    color : djsadmin.theme.Color,
+    decoration : djsadmin.theme.Decoration,
+    font : djsadmin.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : djsadmin.theme.Appearance
+  }
+});
